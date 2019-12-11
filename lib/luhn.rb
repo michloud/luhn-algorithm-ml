1400e6ae95ac439c103bb85bdbd1c70895d845e1
michloud <chinoah96@gmail.com>
2019-12-11 00:43:38 +0000

    add validation for cardlength between 15-16

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -4,6 +4,7 @@ module Luhn
     sum = 0
     num = number.to_s.split("")
     num.reverse.each_with_index do |dig, ind|
+      return false unless num.length.between?(15, 16)
       x = dig.to_i
       x *= 2 if ind.odd?
       x -= 9 if x >= 10
@@ -15,5 +16,7 @@ module Luhn
   end
 end
 
+puts Luhn.is_valid?(1115)
 
+ 
 
